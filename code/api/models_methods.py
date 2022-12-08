1a436b7b82c9147c230babbe32d4edfaa5bfbbdb
Matcraft94 <mat.eduardo.arias@outlook.com>
2022-12-08 11:50:42 -0500

    Create models_methods.py

---
+++ code/api/models_methods.py
@@ -0,0 +1,342 @@
+import torch
+from torch import Tensor
+from torch import nn
+from torch.nn  import functional as F 
+from torch.autograd import Variable
+
+# Modelos y metodos
+class ODEF(nn.Module):
+    def forward_with_grad(self, z, t, grad_outputs):
+        """Compute f and a df/dz, a df/dp, a df/dt"""
+        batch_size = z.shape[0]
+
+        out = self.forward(z, t)
+
+        a = grad_outputs
+        adfdz, adfdt, *adfdp = torch.autograd.grad(
+            (out,), (z, t) + tuple(self.parameters()), grad_outputs=(a),
+            allow_unused=True, retain_graph=True
+        )
+        # el metodo grad automaticamete suma los gradientes para los items del lote, tenemos que expandirlos
+        if adfdp is not None:
+            adfdp = torch.cat([p_grad.flatten() for p_grad in adfdp]).unsqueeze(0)
+            adfdp = adfdp.expand(batch_size, -1) / batch_size
+        if adfdt is not None:
+            adfdt = adfdt.expand(batch_size, 1) / batch_size
+        return out, adfdz, adfdt, adfdp
+
+    def flatten_parameters(self):
+        p_shapes = []
+        flat_parameters = []
+        for p in self.parameters():
+            p_shapes.append(p.size())
+            flat_parameters.append(p.flatten())
+        return torch.cat(flat_parameters)
+
+class ODEAdjoint(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, z0, t, flat_parameters, func):
+        assert isinstance(func, ODEF)
+        bs, *z_shape = z0.size()
+        time_len = t.size(0)
+
+        with torch.no_grad():
+            z = torch.zeros(time_len, bs, *z_shape).to(z0)
+            z[0] = z0
+            for i_t in range(time_len - 1):
+                z0 = ode_solve(z0, t[i_t], t[i_t+1], func)
+                z[i_t+1] = z0
+
+        ctx.func = func
+        ctx.save_for_backward(t, z.clone(), flat_parameters)
+        return z
+
+    @staticmethod
+    def backward(ctx, dLdz):
+        """
+        dLdz shape: time_len, batch_size, *z_shape
+        """
+        func = ctx.func
+        t, z, flat_parameters = ctx.saved_tensors
+        time_len, bs, *z_shape = z.size()
+        n_dim = np.prod(z_shape)
+        n_params = flat_parameters.size(0)
+
+        # La dinámica del sistema aumentado se calculará hacia atrás en el tiempo
+        def augmented_dynamics(aug_z_i, t_i):
+            """
+            los tensores aquí son cortes temporales
+            t_i - es un tensor con tamano: bs, 1
+            aug_z_i - es un tensor con tamano: bs, n_dim*2 + n_params + 1
+            """
+            z_i, a = aug_z_i[:, :n_dim], aug_z_i[:, n_dim:2*n_dim]  # ignorar parámetros y tiempo
+
+            # desdoblar z y a
+            z_i = z_i.view(bs, *z_shape)
+            a = a.view(bs, *z_shape)
+            with torch.set_grad_enabled(True):
+                t_i = t_i.detach().requires_grad_(True)
+                z_i = z_i.detach().requires_grad_(True)
+                func_eval, adfdz, adfdt, adfdp = func.forward_with_grad(z_i, t_i, grad_outputs=a)  # bs, *z_shape
+                adfdz = adfdz.to(z_i) if adfdz is not None else torch.zeros(bs, *z_shape).to(z_i)
+                adfdp = adfdp.to(z_i) if adfdp is not None else torch.zeros(bs, n_params).to(z_i)
+                adfdt = adfdt.to(z_i) if adfdt is not None else torch.zeros(bs, 1).to(z_i)
+
+            # Aplanamos f y adfdz
+            func_eval = func_eval.view(bs, n_dim)
+            adfdz = adfdz.view(bs, n_dim) 
+            return torch.cat((func_eval, -adfdz, -adfdp, -adfdt), dim=1)
+
+        dLdz = dLdz.view(time_len, bs, n_dim)  # aplanamos dLdz por conveniencia
+        with torch.no_grad():
+            ## Cramos placeholders para las salidas de los gradientes
+            # Prev calcula adjuntos hacia atras para ser ajustados por los gradientes directos
+            adj_z = torch.zeros(bs, n_dim).to(dLdz)
+            adj_p = torch.zeros(bs, n_params).to(dLdz)
+            # En contraste a z y p, necesitamos retornar gradientes para todos los tiempos
+            adj_t = torch.zeros(time_len, bs, 1).to(dLdz)
+
+            for i_t in range(time_len-1, 0, -1):
+                z_i = z[i_t]
+                t_i = t[i_t]
+                f_i = func(z_i, t_i).view(bs, n_dim)
+
+                # Calcular gradientes directos
+                dLdz_i = dLdz[i_t]
+                dLdt_i = torch.bmm(torch.transpose(dLdz_i.unsqueeze(-1), 1, 2), f_i.unsqueeze(-1))[:, 0]
+
+                # Ajuste de adjuntos con gradientes directos
+                adj_z += dLdz_i
+                adj_t[i_t] = adj_t[i_t] - dLdt_i
+
+                # Empaquetar variable aumentada
+                aug_z = torch.cat((z_i.view(bs, n_dim), adj_z, torch.zeros(bs, n_params).to(z), adj_t[i_t]), dim=-1)
+
+                # Resolver sistema aumentado al revés
+                aug_ans = ode_solve(aug_z, t_i, t[i_t-1], augmented_dynamics)
+
+                # Desempaquetar el sistema aumentado hacia atrás resuelto
+                adj_z[:] = aug_ans[:, n_dim:2*n_dim]
+                adj_p[:] += aug_ans[:, 2*n_dim:2*n_dim + n_params]
+                adj_t[i_t-1] = aug_ans[:, 2*n_dim + n_params:]
+
+                del aug_z, aug_ans
+
+            ## Ajuste 0 tiempo adjunto con gradientes directos
+            # Calculamos los gradientes dierectos
+            dLdz_0 = dLdz[0]
+            dLdt_0 = torch.bmm(torch.transpose(dLdz_0.unsqueeze(-1), 1, 2), f_i.unsqueeze(-1))[:, 0]
+
+            # Ajuste de los adjuntos
+            adj_z += dLdz_0
+            adj_t[0] = adj_t[0] - dLdt_0
+        return adj_z.view(bs, *z_shape), adj_t, adj_p, None
+
+class NeuralODE(nn.Module):
+    def __init__(self, func):
+        super(NeuralODE, self).__init__()
+        assert isinstance(func, ODEF)
+        self.func = func
+
+    def forward(self, z0, t=Tensor([0., 1.]), return_whole_sequence=False):
+        t = t.to(z0)
+        z = ODEAdjoint.apply(z0, t, self.func.flatten_parameters(), self.func)
+        if return_whole_sequence:
+            return z
+        else:
+            return z[-1]
+
+
+#                     Dinamicas
+
+
+class LinearODEF(ODEF):
+    def __init__(self, W):
+        super(LinearODEF, self).__init__()
+        self.lin = nn.Linear(2, 2, bias=False)
+        self.lin.weight = nn.Parameter(W)
+
+    def forward(self, x, t):
+        return self.lin(x)
+
+# Dinamica simple con una matriz
+class SpiralFunctionExample(LinearODEF):
+    def __init__(self):
+        super(SpiralFunctionExample, self).__init__(Tensor([[-0.1, -1.], [1., -0.1]]))
+
+# Dinamica lineal aleatorizada
+class RandomLinearODEF(LinearODEF):
+    def __init__(self):
+        super(RandomLinearODEF, self).__init__(torch.randn(2, 2)/2.)
+
+# Dinamica mas sofisticada para creacion de trayectorias
+class TestODEF(ODEF):
+    def __init__(self, A, B, x0):
+        super(TestODEF, self).__init__()
+        self.A = nn.Linear(2, 2, bias=False)
+        self.A.weight = nn.Parameter(A)
+        self.B = nn.Linear(2, 2, bias=False)
+        self.B.weight = nn.Parameter(B)
+        self.x0 = nn.Parameter(x0)
+
+    def forward(self, x, t):
+        xTx0 = torch.sum(x*self.x0, dim=1)
+        dxdt = torch.sigmoid(xTx0) * self.A(x - self.x0) + torch.sigmoid(-xTx0) * self.B(x + self.x0)
+        return dxdt
+
+# Dinamica a optimizar
+class NNODEF(ODEF):
+    def __init__(self, in_dim, hid_dim, time_invariant=False):
+        super(NNODEF, self).__init__()
+        self.time_invariant = time_invariant
+
+        if time_invariant:
+            self.lin1 = nn.Linear(in_dim, hid_dim)
+        else:
+            self.lin1 = nn.Linear(in_dim+1, hid_dim)
+        self.lin2 = nn.Linear(hid_dim, hid_dim)
+        self.lin3 = nn.Linear(hid_dim, in_dim)
+        self.elu = nn.ELU(inplace=True)
+
+    def forward(self, x, t):
+        if not self.time_invariant:
+            x = torch.cat((x, t), dim=-1)
+
+        h = self.elu(self.lin1(x))
+        h = self.elu(self.lin2(h))
+        out = self.lin3(h)
+        return out
+
+
+
+# Definimos capas
+def norm(dim):
+    return nn.BatchNorm2d(dim)
+
+def conv3x3(in_feats, out_feats, stride=1):
+    return nn.Conv2d(in_feats, out_feats, kernel_size=3, stride=stride, padding=1, bias=False)
+
+def add_time(in_tensor, t):
+    bs, c, w, h = in_tensor.shape
+    return torch.cat((in_tensor, t.expand(bs, 1, w, h)), dim=1)
+
+
+#  Red CNN
+class ConvODEF(ODEF):
+    def __init__(self, dim):
+        super(ConvODEF, self).__init__()
+        self.conv1 = conv3x3(dim + 1, dim)
+        self.norm1 = norm(dim)
+        self.conv2 = conv3x3(dim + 1, dim)
+        self.norm2 = norm(dim)
+
+    def forward(self, x, t):
+        xt = add_time(x, t)
+        h = self.norm1(torch.relu(self.conv1(xt)))
+        ht = add_time(h, t)
+        dxdt = self.norm2(torch.relu(self.conv2(ht)))
+        return dxdt
+
+
+# MINSTC continuo
+class ContinuousNeuralMNISTClassifier(nn.Module):
+    def __init__(self, ode):
+        super(ContinuousNeuralMNISTClassifier, self).__init__()
+        self.downsampling = nn.Sequential(
+            nn.Conv2d(1, 64, 3, 1),
+            norm(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 4, 2, 1),
+            norm(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 4, 2, 1),
+        )
+        self.feature = ode
+        self.norm = norm(64)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = self.downsampling(x)
+        x = self.feature(x)
+        x = self.norm(x)
+        x = self.avg_pool(x)
+        shape = torch.prod(torch.tensor(x.shape[1:])).item()
+        x = x.view(-1, shape)
+        out = self.fc(x)
+        return out
+
+
+# Autoencoder RNN
+class RNNEncoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(RNNEncoder, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+
+        self.rnn = nn.GRU(input_dim+1, hidden_dim)
+        self.hid2lat = nn.Linear(hidden_dim, 2*latent_dim)
+
+    def forward(self, x, t):
+        # Concatenar tiempo para ingresar
+        t = t.clone()
+        t[1:] = t[:-1] - t[1:]
+        t[0] = 0.
+        xt = torch.cat((x, t), dim=-1)
+
+        _, h0 = self.rnn(xt.flip((0,)))  # Invertida
+        # Calcular dimensión latente
+        z0 = self.hid2lat(h0[0])
+        z0_mean = z0[:, :self.latent_dim]
+        z0_log_var = z0[:, self.latent_dim:]
+        return z0_mean, z0_log_var
+
+# EDO Neuronal DEcodificadora
+class NeuralODEDecoder(nn.Module):
+    def __init__(self, output_dim, hidden_dim, latent_dim):
+        super(NeuralODEDecoder, self).__init__()
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+
+        func = NNODEF(latent_dim, hidden_dim, time_invariant=True)
+        self.ode = NeuralODE(func)
+        self.l2h = nn.Linear(latent_dim, hidden_dim)
+        self.h2o = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, z0, t):
+        zs = self.ode(z0, t, return_whole_sequence=True)
+
+        hs = self.l2h(zs)
+        xs = self.h2o(hs)
+        return xs
+
+
+# VAE EDO
+class ODEVAE(nn.Module):
+    def __init__(self, output_dim, hidden_dim, latent_dim):
+        super(ODEVAE, self).__init__()
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+
+        self.encoder = RNNEncoder(output_dim, hidden_dim, latent_dim)
+        self.decoder = NeuralODEDecoder(output_dim, hidden_dim, latent_dim)
+
+    def forward(self, x, t, MAP=False):
+        z_mean, z_log_var = self.encoder(x, t)
+        if MAP:
+            z = z_mean
+        else:
+            z = z_mean + torch.randn_like(z_mean) * torch.exp(0.5 * z_log_var)
+        x_p = self.decoder(z, t)
+        return x_p, z, z_mean, z_log_var
+
+    def generate_with_seed(self, seed_x, t):
+        seed_t_len = seed_x.shape[0]
+        z_mean, z_log_var = self.encoder(seed_x, t[:seed_t_len])
+        x_p = self.decoder(z_mean, t)
+        return x_p
+
+
