9efe7145441ea4912c5c9c84b5d430e9f9ab2c7f
Matcraft94 <mat.eduardo.arias@outlook.com>
2022-12-09 21:50:23 -0500

    Update
    Actualizacion final antes de presentar

--- code/api/utils_EDO_Neuronal.py
+++ code/api/utils_EDO_Neuronal.py
@@ -367,3 +367,72 @@ class ContinuousNeuralMNISTClassifier(nn.Module):
         return out
 
 
+class RNNEncoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(RNNEncoder, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+
+        self.rnn = nn.GRU(input_dim+1, hidden_dim)
+        self.hid2lat = nn.Linear(hidden_dim, 2*latent_dim)
+
+    def forward(self, x, t):
+        # Concatenar tiempo para ingresar
+        t = t.clone()
+        t[1:] = t[:-1] - t[1:]
+        t[0] = 0.
+        xt = torch.cat((x, t), dim=-1)
+
+        _, h0 = self.rnn(xt.flip((0,)))  # Invertida
+        # Calcular dimensión latente
+        z0 = self.hid2lat(h0[0])
+        z0_mean = z0[:, :self.latent_dim]
+        z0_log_var = z0[:, self.latent_dim:]
+        return z0_mean, z0_log_var
+
+class NeuralODEDecoder(nn.Module):
+    def __init__(self, output_dim, hidden_dim, latent_dim):
+        super(NeuralODEDecoder, self).__init__()
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+
+        func = NNODEF(latent_dim, hidden_dim, time_invariant=True)
+        self.ode = NeuralODE(func)
+        self.l2h = nn.Linear(latent_dim, hidden_dim)
+        self.h2o = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, z0, t):
+        zs = self.ode(z0, t, return_whole_sequence=True)
+
+        hs = self.l2h(zs)
+        xs = self.h2o(hs)
+        return xs
+
+
+class ODEVAE(nn.Module):
+    def __init__(self, output_dim, hidden_dim, latent_dim):
+        super(ODEVAE, self).__init__()
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+
+        self.encoder = RNNEncoder(output_dim, hidden_dim, latent_dim)
+        self.decoder = NeuralODEDecoder(output_dim, hidden_dim, latent_dim)
+
+    def forward(self, x, t, MAP=False):
+        z_mean, z_log_var = self.encoder(x, t)
+        if MAP:
+            z = z_mean
+        else:
+            z = z_mean + torch.randn_like(z_mean) * torch.exp(0.5 * z_log_var)
+        x_p = self.decoder(z, t)
+        return x_p, z, z_mean, z_log_var
+
+    def generate_with_seed(self, seed_x, t):
+        seed_t_len = seed_x.shape[0]
+        z_mean, z_log_var = self.encoder(seed_x, t[:seed_t_len])
+        x_p = self.decoder(z_mean, t)
+        return x_p
+
